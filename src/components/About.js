37ba12ac6ed6942150e2b790a3362099f8b59da3
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-05-04 17:57:23 -0300

    Add about me section

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <h1 className="about-me-title">About Me</h1>
+            <p className="about-me-paragraph">My name is Lauro Aranha, I am a Software Developer with one year of professional experience developing and delivering software.
+                <br />Throughout my carrer, I have experienced working with industry solutions based projects, using Frontend, Backend techologies and tools, such as <b>Java</b>, <b>Javascript</b>, <b>DB2</b> and <b>React</b>, alongside with <b>Scrum Agile Methodology</b>.
+                <br />When I am not coding, I like playing some online games with friends.
+            </p>
+        </div >
+    )
+}
+
+export default About;
